044e7de1a81afeaf3a7956cfcd4b3d9029d06646
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2021-07-30 18:06:57 +0900

    fix svl2py

--- docs/svl2py.py
+++ docs/svl2py.py
@@ -5,21 +5,35 @@ p = ArgumentParser()
 p.add_argument("svl")
 a = p.parse_args()
 
-re_docs_header = re.compile("^/\*(.+?)\*/", re.MULTILINE | re.DOTALL)
-re_function = re.compile("function (.+?) +(.+?)\n.*?/\*(.+?)\*/.+?endfunction", re.MULTILINE | re.DOTALL)
-
+data = []
+level = ""
 with open(a.svl, "r") as f:
-    body = f.read()
-    body = re.sub("function .+;", "", body)
+    for line in f:
+        line = line.strip()
+        if "function " in line:
+            if ";" in line:
+                continue
+            level = "\t"
+            temp = line.split("function ")[1].strip()
+            func_title = temp.split(" ")[0]
+            func_attr = temp.replace(func_title, "").strip(" []")
+            data.append(f"def {func_title} ({func_attr}):")
+            
+        if "/*" in line:
+            data.append(level + '"""' + line.replace("/*", ""))
+            for line_doc in f:
+                line_doc = line_doc.strip()
+                if "*/" in line_doc:
+                    data.append(level + line_doc.replace("*/", ""))
+                    data.append(level + '"""')
+                    break
+                data.append(level + line_doc)
+        
+        if "endfunction" in line:
+            data.append("\tpass")
+            level = ""
 
 with open(a.svl.replace(".svl", ".py"), "w") as f:
-    try:
-        header = next(re_docs_header.finditer(body))
-        f.write('"""' + header[1] + '\n"""\n')
-    except:
-        pass
+    f.write("\n".join(data))
 
-    for line in re_function.findall(body):
-        f.write("def " + line[0] + "(" + line[1].strip("[]") + "):")
-        f.write('\n\t"""' + line[2] + '\n\t"""\n\tpass\n')
 
